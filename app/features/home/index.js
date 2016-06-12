fc0f37ece5c3fa0ad60f5ed833f74b54290f3aeb
Dawid <dawid.pieprzyca@gmail.com>
2016-06-12 07:41:02 +0000

    Something strange

--- app/features/home/index.js
+++ app/features/home/index.js
@@ -1,10 +1,11 @@
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
-
+import firebase from 'firebase';
+import angularfire from 'angularfire';
 import routing from './home.routes';
 import HomeController from './home.controller';
 
-export default angular.module('app.home', [uirouter])
+export default angular.module('app.home', [uirouter], ["firebase"])
   .config(routing)
   .controller('HomeController', HomeController)
   .name;
